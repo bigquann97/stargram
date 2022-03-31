a3dcfa2bcc3461c08a2fdb2fbd212d0a8f61048c
bigquann97 <bigquan977@gmail.com>
2022-04-01 00:24:01 +0900

    init project

---
+++ src/test/java/demo/stargram/StargramApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.stargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
